23540e972a2345a42a213de89c3a96a16ec6bbf8
SafaeQ <safaedani009@gmail.com>
2021-02-28 19:31:13 +0100

    edit login

--- code/logout.php
+++ code/logout.php
@@ -105,6 +105,16 @@
         </tr>
        
       </table>
+
+
+      <?php
+      
+      if(isset($_GET['logout'])) {
+        session_destroy();
+        unset($_SESSION['username']);
+        header('location:login.php');
+    }
+    ?>
     
 </body>
 </html>
