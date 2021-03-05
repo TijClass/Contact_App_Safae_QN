93a3b006bbe83f1d431c68b74043ad079d20f52c
SafaeQ <safaedani009@gmail.com>
2021-03-05 16:30:53 +0100

    adding insert and select

--- code/contact.php
+++ code/contact.php
@@ -5,6 +5,30 @@ if(!isset($_SESSION["user"])){
 }
 
 ?>
+ <?php 
+    if(isset($_POST['send'])){
+        if(!empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['Adress']) && !empty($_POST['Phone']) && !empty($_POST['email']) && !empty($_POST['unit']) && !empty($_POST['area'])) {
+                 $first_name = $_POST['fname'];
+                  $last_name = $_POST['lname'];
+                  $address1 = $_POST['Adress'];
+                  $phone = $_POST['Phone'];
+                  $email = $_POST['email'];
+                  $group = $_POST['unit'];
+                  $notes = $_POST['area'];
+                if(mysqli_query($conn,"INSERT INTO contacts (`first_name`, `last_name`, `address1`, `phone`, `email`, `group`,`notes`)
+                VALUES ('$first_name', '$last_name', '$address1', '$phone',' $email', '$group', '$notes')"))
+                
+              {
+                echo "form is succesefully";
+                header("reflech:2,url=contact.php");
+              } else {
+                echo "form not submitted";
+              }
+      }else{
+        die('test');
+      }
+    }  
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -45,8 +69,7 @@ if(!isset($_SESSION["user"])){
         </div>
     </div>
     
-    <table >
-      <tbody>
+    <table >      
         <tr class="line1">
           <th>Id</th>
           <th>FirstName</th>
@@ -57,7 +80,30 @@ if(!isset($_SESSION["user"])){
           <th>Groupe</th>
           <th>Actions</th>
         </tr>
-        <tr class="line2">
+        <?php
+          $sql = "SELECT * FROM contacts";
+          $result = $conn->query($sql);
+          
+          if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {                          
+        ?>
+        <tr class="line1">
+          <td><?=$row["id"]?></td>
+          <td><?=$row["first_name"]?></td>
+          <td><?=$row["last_name"]?></td>
+          <td><?=$row["email"]?></td>
+          <td><?=$row["address1"]?></td>
+          <td><?=$row["group"]?></td>
+          <td><?=$row["notes"]?></td>
+          <td><span>Edit</span>|<span>Delete</span></td>
+        </tr>
+          <?php
+          }
+          }
+          ?>
+        
+        <!-- <tr class="line2">
           <td>58011</td>
           <td>Anaa</td>
           <td>jennys</td>
@@ -111,8 +157,16 @@ if(!isset($_SESSION["user"])){
           <td>Friend</td>
           <td><a href="www.google.com">Edit <span class="drop">X</span></a></td>
          
-        </tr>
-       
+        </tr> -->
+        <?php
+        
+    //   if(mysqli_query($conn, $sql)){
+    //     echo "Table created successfully.";
+    // } else{
+    //     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    // }
+        ?>
+     
       </table>
 </div>
 <div id="id01" class="modal" style="display: none;position: absolute; top: 0; left: 0; width: 80vw; height: 95vh; margin: 2.5vh 10vw;">
@@ -126,42 +180,41 @@ if(!isset($_SESSION["user"])){
     <form method="POST" action="contact.php">
         <div class="formgroup">
             <label for="fname">First name:</label>
-            <input type="text" id="fname" name="fname" placeholder="Enter your first name">
+            <input type="text" id="fname" name="fname" placeholder="Enter your first name" required>
         </div>
         <div class="formgroup ">
             <label for="lname">Last name:</label>
-            <input type="text" id="lname" name="lname" placeholder="Enter your last name">
+            <input type="text" id="lname" name="lname" placeholder="Enter your last name" required>
         </div>
         <div class="formgroup ">
             <label for="lname">Adress:</label>
-            <input type="text" id="lname" name="Adress" placeholder="Enter your Adress">
+            <input type="text" id="lname" name="Adress" placeholder="Enter your Adress" required>
         </div><div class="formgroup one">
             <label for="lname">Phone:</label>
-            <input type="number" id="Pname" name="Pname" placeholder="Enter your Phone Number">
+            <input type="number" id="Pname" name="Phone" placeholder="Enter your Phone Number" required>
         </div>
         <div class="formgroup ">
             <label for="email">Email :</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email">
+            <input type="email" id="email" name="email" placeholder="Enter your email" required>
         </div>
             <div class="radios">
-             <input type="radio" name="unit1" value="">Familly
-             <input type="radio" name="unit2" value="">Friend
-            <input type="radio" name="unit3" value="">Businness
+             <input type="radio" name="unit" value="Familly" required>Familly
+             <input type="radio" name="unit" value="Friend" required>Friend
+             <input type="radio" name="unit" value="Businness" required>Businness
             </div>
         <div class="formgroup ">
             <label for="lname">Notes:</label>
-           <textarea  name="area" id="area" placeholder="Enter your Notes" ></textarea> 
+           <textarea  name="area" id="area" placeholder="Enter your Notes" required></textarea> 
         </div>
         
-    </div>
+    <!-- </div> -->
         <div class="formgroup ">
-            <button class="btn3" name="send" type="submit" form="nameform" value="Send">Submit</button>
+            <input class="btn3" name="send" type="submit" value="Send"></input>
         </div>
             </div>
           </div>
         </div>
-        </form>
-     
+    </form>
     
 </body>
 </html>
--- code/functions.php
+++ code/functions.php
@@ -9,6 +9,7 @@
  if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 //  if($conn){
 //      echo "connected";
 //  }else{
@@ -20,17 +21,6 @@
 
 
 
-// if(isset($_POST['send'])){
-//     $first_name = $_POST['fname'];
-//     $last_name = $_POST['lname'];
-//     $address1 = $_POST['Adress'];
-//     $phone = $_POST['Pname'];
-//     $email = $_POST['email'];
-//     $group = $_POST['group'];
-//     $notes = $_POST['area'];
-//     $query = "INSERT INTO `contacts`('first_name','last_name','address1','phone','email','group','notes')VALUE('$first_name','$last_name','$address1','$phone','$email','$group','$notes')";
-//     mysqli_query($conn,$query);
 
-// }
 
 ?>
